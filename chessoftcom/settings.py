561c2aa9d4bfd13ddb84b8e54a9a56f1c1b7aa00
Игнат Сергеев <ignat.sergeev@softcom.su>
2025-05-27 18:36:31 +0000

    ci

--- chessoftcom/settings.py
+++ chessoftcom/settings.py
@@ -56,10 +56,12 @@ WSGI_APPLICATION = 'chessoftcom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DB_PATH = os.environ.get("DJANGO_DB_PATH", ".")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / DB_PATH / 'db.sqlite3',
     }
 }
 
